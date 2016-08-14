53363cb0c1e4bf874a4e7e6f2d645c9a89fd7566
JananiSivakumar <joseiyer@gmail.com>
2016-08-13 21:50:53 -0700

    Create absex.java

---
+++ src/abstract/absex.java
@@ -0,0 +1,8 @@
+package abstracts;
+
+public abstract class absex {
+void methabs1(){
+	System.out.println("Method has a body");
+}
+public abstract void methabs2();// A method which has no body is an abstract method
+}
