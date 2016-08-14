93f8c58aabcd5fa64de71bd22b0479bf481f8958
JananiSivakumar <joseiyer@gmail.com>
2016-08-13 22:01:38 -0700

    Delete absex.java

--- src/abstract/absex.java
+++
@@ -1,8 +0,0 @@
-package abstracts;
-
-public abstract class absex {
-void methabs1(){
-	System.out.println("Method has a body");
-}
-public abstract void methabs2();// A method which has no body is an abstract method
-}
